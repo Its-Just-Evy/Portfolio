c94bc37e5b15406ab60a8ae9ce1f0968cbb8e818
Evy <evylukassen@hotmail.com>
2020-12-08 10:07:27 +0100

    all context is filled
    all the context that needs to be on the website is on the website

--- public/index.js
+++ public/index.js
@@ -1,12 +1,26 @@
 var boomies = document.getElementById("Boomies");
-// var axi = document.getElementById(AXI);
+var axi = document.getElementById("AXI");
 
 function showBoomies() {
     boomies.classList.remove("hidden")
     boomies.classList.add("flex")
+    axi.classList.remove("flex")
+    axi.classList.add("hidden")
 }
 
 function noshowBoomies() {
-    boomies.classList.remove("block")
+    boomies.classList.remove("flex")
     boomies.classList.add("hidden")
+}
+
+function showAxi() {
+    axi.classList.remove("hidden")
+    axi.classList.add("flex")
+    boomies.classList.remove("flex")
+    boomies.classList.add("hidden")
+}
+
+function noshowAxi() {
+    axi.classList.remove("flex")
+    axi.classList.add("hidden")
 }
