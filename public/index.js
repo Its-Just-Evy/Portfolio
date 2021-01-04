2abd30b1126f520d0ee67334927f3edce2b22cec
Evy <evylukassen@hotmail.com>
2021-01-04 11:25:08 +0100

    scroll fixed and animation added
    The croll has been fixed and the content now looks good on the page. An animation has been added that types when you get on the page

--- public/index.js
+++ public/index.js
@@ -80,3 +80,20 @@ $(window).load(function(){
         scrollFooter(scroll, footerHeight);
     }
 });
+
+// typing animation
+(function ($) {
+    $.fn.writeText = function (content) {
+      var contentArray = content.split(""),
+        current = 0,
+        elem = this;
+      setInterval(function () {
+        if (current < contentArray.length) {
+          elem.text(elem.text() + contentArray[current++]);
+        }
+      }, 80);
+    };
+  })(jQuery);
+
+  // input text for typing animation
+  $("#holder").writeText("FRONT-END DEVELOPER");
